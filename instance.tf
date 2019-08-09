5476163c351d91de8408e04925199443f9df7c71
csamatov96 <996bkg@gmail.com>
2019-08-09 18:27:49 -0500

    user_data was added in instance.tf file module

--- instance.tf
+++ instance.tf
@@ -3,13 +3,14 @@ resource "aws_instance" "test_ec2" {
     instance_type = "t2.micro" #hardcoded
     key_name = "${aws_key_pair.Terraform_machine.key_name}" #full path 
     security_groups = ["${aws_security_group.my_custom_sec_group.name}"] #full path 
+    user_data = "${file("userdata.sh")}"
     #count = "2"
 
     tags = {
-    Name = "test_env"
-    Dept = "DevOps"
-    Group = "April"
-    Created_by = "JC"
-  }
+        Name = "test_env"
+        Dept = "DevOps"
+        Group = "April"
+        Created_by = "JC"
+    }
 
 }
