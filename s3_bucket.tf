cfc383d844554f3129dcf3fb88f79e7939393147
csamatov96 <996bkg@gmail.com>
2019-08-09 14:52:39 -0500

    region added

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "b" { #is used for Terraform 
   bucket = "my_tf_test_bucket_chyngyzkan" #an actual name
   acl    = "private"
+  region = "us-east-2" 
 
   tags = {
     Name = "test_env"
