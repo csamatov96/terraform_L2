471b2748f068f14ec6856e2b555dac2bec055fa9
csamatov96 <996bkg@gmail.com>
2019-08-09 18:53:26 -0500

    work!!!!

--- state_storage.tf
+++ state_storage.tf
@@ -1,5 +1,5 @@
 terraform {
-    state_storage "s3" {
+    backend "s3" {
         bucket = "terraform-state-storage-chyngyzkan"
         region = "us-east-2"
         key = "infrastructure.tfstate"
