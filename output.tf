a7db5049c97d14ac7fb2b1c12545f38eee978e01
csamatov96 <996bkg@gmail.com>
2019-08-09 17:46:54 -0500

    bucket instance output added

--- output.tf
+++ output.tf
@@ -1,3 +1,12 @@
 output "key_name" { #for Terraform 
   value = "${aws_key_pair.Terraform_machine.key_name}" #will show after aplying public_key, to avoid it add one more option
 }
+
+output "bucketname" {
+  value = "${aws_s3_bucket.b.bucket}" 
+}
+
+output "instance" {
+  value = "${aws_instance.test_ec2.id}"
+}
+
