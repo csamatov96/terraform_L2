d2af35cf1f8e4b3851142dd9c5321ca48d9cb9f5
csamatov96 <996bkg@gmail.com>
2019-08-09 18:50:42 -0500

    state_storage was replaced

--- state_storage.tf
+++ state_storage.tf
@@ -1,5 +1,5 @@
 terraform {
-    backend "s3" {
+    state_storage "s3" {
         bucket = "terraform-state-storage-chyngyzkan"
         region = "us-east-2"
         key = "infrastructure.tfstate"
