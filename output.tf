fff9b88e9fecaa090bb8fd10e3f8625a7a3b849b
csamatov96 <996bkg@gmail.com>
2019-08-09 17:58:02 -0500

    public ip and username added to output.tf

--- output.tf
+++ output.tf
@@ -6,7 +6,16 @@ output "bucketname" {
   value = "${aws_s3_bucket.b.bucket}" 
 }
 
-output "instance" {
+output "ID" {
   value = "${aws_instance.test_ec2.id}"
 }
+
+output "public IP" {
+  value = "${aws_instance.test_ec2.public_ip}" #
+}
+
+output "username" {
+  value = "ec-user" #hardcoded, any text can be written like ec2-user and it'll be showen
+}
+
 
