aeb38fa20cd3cdaeb618a4eca0d7491d491442b1
csamatov96 <996bkg@gmail.com>
2019-08-09 17:40:51 -0500

    key_name option added to output.tf so public key will not be showen everytime

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "key_name" { #for Terraform 
-  value = "${aws_key_pair.Terraform_machine}" #for me
+  value = "${aws_key_pair.Terraform_machine.key_name}" #will show after aplying public_key, to avoid it add one more option
 }
