50125e9dc552f17171a5a0b6b14aa42e8ef9fadb
csamatov96 <996bkg@gmail.com>
2019-08-09 18:49:28 -0500

    bucket was misspelled

--- state_storage.tf
+++ state_storage.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        backend = "terraform-state-storage-chyngyzkan"
+        bucket = "terraform-state-storage-chyngyzkan"
         region = "us-east-2"
         key = "infrastructure.tfstate"
     }
