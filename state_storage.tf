358bba6e1cb923ffb003f5735c2ab6d84b68aa0d
csamatov96 <996bkg@gmail.com>
2019-08-09 20:06:17 -0500

    all pushed

--- state_storage.tf
+++ state_storage.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "terraform-state-storage-chyngyzkan"
-        region = "us-east-1"
-        key = "infrastructure.tfstate"
+        region = "us-east-1" #
+        key = "infrastructure.tfstate" #was taken from terraform.tfstate
     }
 }
