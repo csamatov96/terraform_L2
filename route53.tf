491974f45ac479795e8f8333865b70aeabaed1f1
csamatov96 <996bkg@gmail.com>
2019-08-10 23:46:41 -0500

    route53 domain name is changed

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
-resource "aws_route53_record" "test" {
-  zone_id = "ZSBY94ENV7WM4 " #ZSBY94ENV7WM4
-  name    = "test.mydomain.com"
+resource "aws_route53_record" "devops" {
+  zone_id = "Z6XWWD56QDJWX " #Route 53
+  name    = "devops.csamatov.net"
   type    = "A"
   ttl     = "300"
   records = ["${aws_instance.test_ec2.public_ip}"]
