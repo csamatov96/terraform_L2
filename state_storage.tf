cf23c7712b219c21e9d058a1a4db2dc9237b9893
csamatov96 <996bkg@gmail.com>
2019-08-11 00:45:40 -0500

    us-east-2

--- state_storage.tf
+++ state_storage.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         dynamodb_table = "infrastructure_state_lock" #
         bucket = "my-tf-test-bucket-chyngyzkan"
-        region = "us-east-1" #
+        region = "us-east-2" #
         key = "infrastructure.tfstate" #was taken from terraform.tfstate
     }
 }
