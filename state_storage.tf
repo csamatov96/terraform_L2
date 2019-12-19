9875608849353e97c02697dec9e2f0fdd85ed9e6
csamatov96 <csamatov96@gmail.com>
2019-12-19 16:17:20 -0600

    changes

--- state_storage.tf
+++
@@ -1,8 +0,0 @@
-#terraform {
-#    backend "s3" {
-#        dynamodb_table = "infrastructure_state_lock" #
-#        bucket = "terraform-state-storage-chyngyzkan"
-#        region = "us-east-1" #
-#        key = "infrastructure.tfstate" #was taken from terraform.tfstate
-#    }
-#}
