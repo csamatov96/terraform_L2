4f56756f8e5bc70fe7845e8d521820437030fc93
csamatov96 <csamatov96@gmail.com>
2019-12-19 16:15:16 -0600

    changes

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket" "b" { #is used for Terraform 
-  bucket = "terraform-state-storage-chyngyzkan" #variable can't be used
-  acl    = "private"
+#resource "aws_s3_bucket" "b" { #is used for Terraform 
+#  bucket = "terraform-state-storage-chyngyzkan" #variable can't be used
+#  acl    = "private"
   #region = "us-east-2" 
 
-  tags = {
-    Name = "${var.ENV}"
-        Dept = "${var.Dept}"
-        Created_by = "${var.Created_by}"
-    }
+ # tags = {
+ #   Name = "${var.ENV}"
+ ##       Dept = "${var.Dept}"
+    #    Created_by = "${var.Created_by}"
+   # }
 }
--- state_storage.tf
+++ state_storage.tf
@@ -1,8 +1,8 @@
-terraform {
-    backend "s3" {
-        dynamodb_table = "infrastructure_state_lock" #
-        bucket = "terraform-state-storage-chyngyzkan"
-        region = "us-east-1" #
-        key = "infrastructure.tfstate" #was taken from terraform.tfstate
-    }
-}
+#terraform {
+#    backend "s3" {
+#        dynamodb_table = "infrastructure_state_lock" #
+#        bucket = "terraform-state-storage-chyngyzkan"
+#        region = "us-east-1" #
+#        key = "infrastructure.tfstate" #was taken from terraform.tfstate
+#    }
+#}
